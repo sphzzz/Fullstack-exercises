0145cab5953a95363633c38185ac7a0bfc179b4d
sphzzz <165125801+sphzzz@users.noreply.github.com>
2024-08-31 23:11:10 +0800

    1.1,1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,44 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  );
+};
+
+const Total = ({ exercises }) => {
+  return <p>Number of exercises {exercises.reduce((acc, curr) => acc + curr, 0)}</p>;
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total exercises={parts.map(part => part.exercises)} />
+    </div>
+  );
+};
+
+export default App;
