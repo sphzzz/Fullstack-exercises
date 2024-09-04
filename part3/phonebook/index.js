e3e655c4052cd457e0b71bce6fd9e2eaaecc8f0f
sphzzz <165125801+sphzzz@users.noreply.github.com>
2024-09-04 19:30:11 +0800

    3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const currentDate = new Date();
+app.use(express.json())
 
 const options = {
     timeZoneName: 'short'
@@ -48,6 +49,45 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end()
   }
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = Math.floor(Math.random()*9999999999)
+  return String(maxId)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+/*    if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number missing' 
+    })
+  }  */
+
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
