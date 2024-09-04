0daf3badbb2e6ccf83031c7c2a5b712faa03dc1c
sphzzz <165125801+sphzzz@users.noreply.github.com>
2024-09-04 14:53:27 +0800

    3.1,3.2

---
+++ part3/phonebook/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const currentDate = new Date();
+
+const options = {
+    timeZoneName: 'short'
+};
+
+const formatter = new Intl.DateTimeFormat('en-US', options);
+let persons = [
+    {
+      id: 1,
+      name: "Arto Hellas",
+      number: "040-123456"
+    },
+    {
+      id: 2,
+      name: "Ada Lovelace",
+      number: "39-44-5323523"
+    },
+    {
+      id: 3,
+      name: "Dan Abramov",
+      number: "12-43-234345"
+    },
+    {
+      id: 4,
+      name: "Mary Poppendieck",
+      number: "39-23-6423122"
+    }
+
+]
+
+app.get('/info', (request, response) => {
+  response.send(
+    'Phonebook has info for ' + persons.length + ` people<br>` + 
+    `${currentDate.toString()}`
+  )})
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
