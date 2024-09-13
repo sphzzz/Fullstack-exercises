ae1e2e132a3ae79bc647342a7690e80bebb631e3
sphzzz <165125801+sphzzz@users.noreply.github.com>
2024-09-13 23:43:15 +0800

    5.4

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,150 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [notification, setNotification] = useState(null)
+  const [newBlog, setNewBlog] = useState({ title: '', author: '', url: '' })
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+      blogService.getAll().then(blogs =>
+        setBlogs(blogs)
+      )
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      setNotification('Login successful')
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+    } catch (exception) {
+      setNotification('Wrong credentials')
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser(null)
+    setBlogs([])
+    setNotification('Logout successful')
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
+  }
+
+  const handleAddBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const blog = await blogService.create(newBlog)
+      setBlogs(blogs.concat(blog))
+      setNewBlog({ title: '', author: '', url: '' })
+      setNotification(`A new blog "${blog.title}" by ${blog.author} added`)
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+    } catch (exception) {
+      setNotification('Error adding blog')
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <Notification message={notification} />
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <Notification message={notification} />
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+      <form onSubmit={handleAddBlog}>
+        <div>
+          title
+          <input
+            type="text"
+            value={newBlog.title}
+            name="Title"
+            onChange={({ target }) => setNewBlog({ ...newBlog, title: target.value })}
+          />
+        </div>
+        <div>
+          author
+          <input
+            type="text"
+            value={newBlog.author}
+            name="Author"
+            onChange={({ target }) => setNewBlog({ ...newBlog, author: target.value })}
+          />
+        </div>
+        <div>
+          url
+          <input
+            type="text"
+            value={newBlog.url}
+            name="Url"
+            onChange={({ target }) => setNewBlog({ ...newBlog, url: target.value })}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
